36df5d6c499ed35d9db1a58cbc2ae9cb0eb10c97
user <user@courseGIT.pl>
2021-01-31 13:19:31 +0100

    added sound attribute

--- sda_exercises_oop_1/cat.py
+++ sda_exercises_oop_1/cat.py
@@ -1,6 +1,7 @@
 class Cat:
-    def __init__(self, name: str):
+    def __init__(self, name: str, sound='meeeooooww'):
         self.name = name
+        self.sound = sound
 
     def make_sound(self) -> str:
-        return f'Name is {self.name} sound is meeeeooooowww'
+        return f'Name is {self.name} sound is {self.sound}'
