3c17a95e9ff6b43edce6abebc05ae34c0dbb913f
user <user@courseGIT.pl>
2021-01-31 13:20:15 +0100

    added sound atr

--- sda_exercises_oop_1/main.py
+++ sda_exercises_oop_1/main.py
@@ -3,10 +3,19 @@ from python_intermediate_training.sda_exercises_oop_1.cat import Cat
 
 def main():
     cat_object = Cat("Tom")
-    sound: str = cat_object.make_sound()
-    print(sound)
-    
-    
+    cat_object_1 = Cat("Curt")
+    cat_object_2 = Cat("Wojtek", "Mial")
+    cat_object_3 = Cat("Lazy", "I don't meow")
+
+    # cats = [cat_object_1, cat_object_2, cat_object_3, cat_object]
+    cats = []
+    cats.append(cat_object)
+    cats.append(cat_object_1)
+    cats.append(cat_object_2)
+    cats.append(cat_object_3)
+    for cat in cats:
+        sound = cat.make_sound()
+        print(sound)
     
 if __name__ == "__main__":
     main()
