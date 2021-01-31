fa4d5f9e2250bb69a6e7408f5c2eb8c23bdb1601
user <user@courseGIT.pl>
2021-01-31 12:26:46 +0100

    sda_exercises_oop_1/main.py

--- sda_exercises_oop_1/main.py
+++ sda_exercises_oop_1/main.py
@@ -4,15 +4,18 @@ class Calculator:
 
     def show_calculator_type(self):
         print(f'calculator type: {self.type_calc}')
+
     @staticmethod
-    def add_two_numbers(a: int, b:int) -> int:
+    def add_two_numbers(a: int, b: int) -> int:
         return a + b
+
 
 def main():
     print(Calculator.add_two_numbers(2, 3))
     calculator = Calculator()
     calculator.show_calculator_type()
     print(calculator.add_two_numbers(2, 5))
+
+
 if __name__ == "__main__":
     main()
-
---
+++ sda_exercises_oop_1/workbook.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+class Osoba(ABC):
+    def __init__(self, imie, wiek):
+        self.imie = imie
+        self.wiek = wiek
+    def __str__(self):
+        return f"{self.imie} ma {self.wiek} lat"
+    @abstractmethod
+    def pokaz_finanse(self):
+        pass
+class Pracownik(Osoba):
+    def __init__(self, imie, wiek, stawka, liczba_godzin):
+        super().__init__(imie, wiek)
+        self.stawka = stawka
+        self.liczba_godzin = liczba_godzin
+    def pokaz_finanse(self):
+        return self.stawka * self.liczba_godzin
+class Student(Osoba):
+    def __init__(self, imie, wiek, stypendium):
+        super().__init__(imie, wiek)
+        self.stypendium = stypendium
+    def pokaz_finanse(self):
+        return self.stypendium
+def main():
+    pracownik = Pracownik('mike', 29, 20, 168)
+    pracownik2 = Pracownik('mike2', 29, 50, 168)
+    student = Student('sarah', 29, 505)
+    student2 = Student('sarah2', 29, 520)
+    person_list = [pracownik, pracownik2, student, student2]
+    for person in person_list:
+        print(person.pokaz_finanse())
