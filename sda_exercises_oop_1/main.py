9cbab280e44220bc32b437c0dde0aca78c7e11dc
user <user@courseGIT.pl>
2021-01-31 10:34:25 +0100

    added function

--- sda_exercises_oop_1/main.py
+++ sda_exercises_oop_1/main.py
@@ -1,5 +1,18 @@
+class Calculator:
+    def __init__(self, type_calc='cisco'):
+        self.type_calc = type_calc
+
+    def show_calculator_type(self):
+        print(f'calculator type: {self.type_calc}')
+    @staticmethod
+    def add_two_numbers(a: int, b:int) -> int:
+        return a + b
+
 def main():
-    print("Hello")
+    print(Calculator.add_two_numbers(2, 3))
+    calculator = Calculator()
+    calculator.show_calculator_type()
+    print(calculator.add_two_numbers(2, 5))
 if __name__ == "__main__":
     main()
 
