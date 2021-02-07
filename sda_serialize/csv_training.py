1adaf0351a7ee8dbaedc57280722851a1c5ce85c
user <user@courseGIT.pl>
2021-02-07 13:40:01 +0100

    added csv read method

--- sda_serialize/csv_training.py
+++ sda_serialize/csv_training.py
@@ -12,12 +12,28 @@ def csv_write(users_list):
         print(f'Exception while writing csv format info: {e.args}')
 
 
+def csv_read():
+    users = []
+    try:
+        with open('./data.csv', 'r') as fd:
+            reader = csv.reader(fd)
+            for row in reader:
+                if row:
+                    users.append(row)
+
+    except (IOError, Exception) as e:
+        print(f'Exception while reading csv format info: {e.args}')
+    return users
+
+
 def main():
     users = [
         ('John', 'Smith', 765438),
         ('Samuel', 'Jackson', 660438)
     ]
     csv_write(users)
+    returned_users = csv_read()
+    print(returned_users)
 
 
 if __name__ == "__main__":
